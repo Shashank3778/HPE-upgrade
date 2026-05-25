8c424a4bdbbd74568cadbd3834e73bb23bc27cbf
Shashank3778 <anku3778@gmail.com>
2026-05-25 15:25:10 +0530

    new desing

--- dist/desktop-header/DesktopHeader.js
+++ dist/desktop-header/DesktopHeader.js
@@ -10,7 +10,6 @@ import { Menu, MenuTrigger, MenuContent } from '../Menu';
 import LogoSlot from '../plugin-slots/LogoSlot';
 import DesktopLoggedOutItemsSlot from '../plugin-slots/DesktopLoggedOutItemsSlot';
 import { desktopLoggedOutItemsDataShape } from './DesktopLoggedOutItems';
-import DesktopMainMenuSlot from '../plugin-slots/DesktopMainMenuSlot';
 import { desktopHeaderMainOrSecondaryMenuDataShape } from './DesktopHeaderMainOrSecondaryMenu';
 import DesktopSecondaryMenuSlot from '../plugin-slots/DesktopSecondaryMenuSlot';
 import DesktopUserMenuSlot from '../plugin-slots/DesktopUserMenuSlot';
@@ -30,6 +29,131 @@ var getPageName = function getPageName(pathname) {
   if (pathname.includes('profile')) return 'Profile';
   if (pathname.includes('account')) return 'Account';
   return 'Dashboard';
+};
+
+// Map menu item label to an SVG icon
+var getIcon = function getIcon() {
+  var label = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : '';
+  var l = label.toLowerCase();
+  if (l.includes('course')) {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "18",
+      height: "18",
+      fill: "none",
+      viewBox: "0 0 24 24",
+      stroke: "currentColor",
+      strokeWidth: "2"
+    }, /*#__PURE__*/React.createElement("path", {
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      d: "M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+    }));
+  }
+  if (l.includes('discover')) {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "18",
+      height: "18",
+      fill: "none",
+      viewBox: "0 0 24 24",
+      stroke: "currentColor",
+      strokeWidth: "2"
+    }, /*#__PURE__*/React.createElement("path", {
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      d: "M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 15.803a7.5 7.5 0 0010.607 0z"
+    }));
+  }
+  if (l.includes('dashboard')) {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "18",
+      height: "18",
+      fill: "none",
+      viewBox: "0 0 24 24",
+      stroke: "currentColor",
+      strokeWidth: "2"
+    }, /*#__PURE__*/React.createElement("path", {
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      d: "M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+    }));
+  }
+  if (l.includes('catalog')) {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "18",
+      height: "18",
+      fill: "none",
+      viewBox: "0 0 24 24",
+      stroke: "currentColor",
+      strokeWidth: "2"
+    }, /*#__PURE__*/React.createElement("path", {
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      d: "M4 6h16M4 10h16M4 14h16M4 18h16"
+    }));
+  }
+  if (l.includes('progress')) {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "18",
+      height: "18",
+      fill: "none",
+      viewBox: "0 0 24 24",
+      stroke: "currentColor",
+      strokeWidth: "2"
+    }, /*#__PURE__*/React.createElement("path", {
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      d: "M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+    }));
+  }
+  if (l.includes('discussion')) {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "18",
+      height: "18",
+      fill: "none",
+      viewBox: "0 0 24 24",
+      stroke: "currentColor",
+      strokeWidth: "2"
+    }, /*#__PURE__*/React.createElement("path", {
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      d: "M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+    }));
+  }
+  if (l.includes('certificate')) {
+    return /*#__PURE__*/React.createElement("svg", {
+      xmlns: "http://www.w3.org/2000/svg",
+      width: "18",
+      height: "18",
+      fill: "none",
+      viewBox: "0 0 24 24",
+      stroke: "currentColor",
+      strokeWidth: "2"
+    }, /*#__PURE__*/React.createElement("path", {
+      strokeLinecap: "round",
+      strokeLinejoin: "round",
+      d: "M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+    }));
+  }
+  // default icon
+  return /*#__PURE__*/React.createElement("svg", {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: "18",
+    height: "18",
+    fill: "none",
+    viewBox: "0 0 24 24",
+    stroke: "currentColor",
+    strokeWidth: "2"
+  }, /*#__PURE__*/React.createElement("path", {
+    strokeLinecap: "round",
+    strokeLinejoin: "round",
+    d: "M13 10V3L4 14h7v7l9-11h-7z"
+  }));
 };
 var DesktopHeader = function DesktopHeader(_ref) {
   var mainMenu = _ref.mainMenu,
@@ -51,7 +175,6 @@ var DesktopHeader = function DesktopHeader(_ref) {
     var location = useLocation();
     pageName = getPageName(location.pathname);
   } catch (e) {
-    // useLocation may not be available in all contexts, fallback to window.location
     pageName = getPageName(window.location.pathname);
   }
   var renderUserMenu = function renderUserMenu() {
@@ -84,6 +207,13 @@ var DesktopHeader = function DesktopHeader(_ref) {
     href: logoDestination
   };
   var logoClasses = getConfig().AUTHN_MINIMAL_HEADER ? 'mw-100' : null;
+  var isActive = function isActive(item) {
+    try {
+      return window.location.pathname.includes(item.href) || item.isActive;
+    } catch (e) {
+      return false;
+    }
+  };
   return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("aside", {
     className: "site-sidebar"
   }, /*#__PURE__*/React.createElement("div", {
@@ -91,8 +221,16 @@ var DesktopHeader = function DesktopHeader(_ref) {
   }, /*#__PURE__*/React.createElement(LogoSlot, logoProps)), /*#__PURE__*/React.createElement("nav", {
     "aria-label": intl.formatMessage(messages['header.label.main.nav']),
     className: "site-sidebar__nav"
-  }, /*#__PURE__*/React.createElement(DesktopMainMenuSlot, {
-    menu: mainMenu
+  }, mainMenu.map(function (item) {
+    return /*#__PURE__*/React.createElement("a", {
+      key: item.href || item.label,
+      href: item.href,
+      className: "site-sidebar__nav-item ".concat(isActive(item) ? 'active' : '')
+    }, /*#__PURE__*/React.createElement("span", {
+      className: "site-sidebar__nav-icon"
+    }, getIcon(item.label)), /*#__PURE__*/React.createElement("span", {
+      className: "site-sidebar__nav-label"
+    }, item.label));
   }))), /*#__PURE__*/React.createElement("header", {
     className: "site-header-desktop site-header-top"
   }, /*#__PURE__*/React.createElement("a", {
--- src/desktop-header/DesktopHeader.jsx
+++ src/desktop-header/DesktopHeader.jsx
@@ -10,7 +10,6 @@ import { Menu, MenuTrigger, MenuContent } from '../Menu';
 import LogoSlot from '../plugin-slots/LogoSlot';
 import DesktopLoggedOutItemsSlot from '../plugin-slots/DesktopLoggedOutItemsSlot';
 import { desktopLoggedOutItemsDataShape } from './DesktopLoggedOutItems';
-import DesktopMainMenuSlot from '../plugin-slots/DesktopMainMenuSlot';
 import { desktopHeaderMainOrSecondaryMenuDataShape } from './DesktopHeaderMainOrSecondaryMenu';
 import DesktopSecondaryMenuSlot from '../plugin-slots/DesktopSecondaryMenuSlot';
 import DesktopUserMenuSlot from '../plugin-slots/DesktopUserMenuSlot';
@@ -30,6 +29,66 @@ const getPageName = (pathname) => {
   if (pathname.includes('profile')) return 'Profile';
   if (pathname.includes('account')) return 'Account';
   return 'Dashboard';
+};
+
+// Map menu item label to an SVG icon
+const getIcon = (label = '') => {
+  const l = label.toLowerCase();
+  if (l.includes('course')) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+      </svg>
+    );
+  }
+  if (l.includes('discover')) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 15.803a7.5 7.5 0 0010.607 0z" />
+      </svg>
+    );
+  }
+  if (l.includes('dashboard')) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+      </svg>
+    );
+  }
+  if (l.includes('catalog')) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 10h16M4 14h16M4 18h16" />
+      </svg>
+    );
+  }
+  if (l.includes('progress')) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+      </svg>
+    );
+  }
+  if (l.includes('discussion')) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+      </svg>
+    );
+  }
+  if (l.includes('certificate')) {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
+      </svg>
+    );
+  }
+  // default icon
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z" />
+    </svg>
+  );
 };
 
 const DesktopHeader = ({
@@ -53,7 +112,6 @@ const DesktopHeader = ({
     const location = useLocation();
     pageName = getPageName(location.pathname);
   } catch (e) {
-    // useLocation may not be available in all contexts, fallback to window.location
     pageName = getPageName(window.location.pathname);
   }
 
@@ -77,9 +135,17 @@ const DesktopHeader = ({
   const logoProps = { src: logo, alt: logoAltText, href: logoDestination };
   const logoClasses = getConfig().AUTHN_MINIMAL_HEADER ? 'mw-100' : null;
 
+  const isActive = (item) => {
+    try {
+      return window.location.pathname.includes(item.href) || item.isActive;
+    } catch (e) {
+      return false;
+    }
+  };
+
   return (
     <>
-      {/* ── SIDEBAR — mainMenu tabs moved here from top nav ── */}
+      {/* ── SIDEBAR ── */}
       <aside className="site-sidebar">
         <div className="site-sidebar__logo">
           <LogoSlot {...logoProps} />
@@ -88,11 +154,20 @@ const DesktopHeader = ({
           aria-label={intl.formatMessage(messages['header.label.main.nav'])}
           className="site-sidebar__nav"
         >
-          <DesktopMainMenuSlot menu={mainMenu} />
+          {mainMenu.map((item) => (
+            <a
+              key={item.href || item.label}
+              href={item.href}
+              className={`site-sidebar__nav-item ${isActive(item) ? 'active' : ''}`}
+            >
+              <span className="site-sidebar__nav-icon">{getIcon(item.label)}</span>
+              <span className="site-sidebar__nav-label">{item.label}</span>
+            </a>
+          ))}
         </nav>
       </aside>
 
-      {/* ── TOP HEADER — page name left, user info right ── */}
+      {/* ── TOP HEADER ── */}
       <header className="site-header-desktop site-header-top">
         <a className="nav-skip sr-only sr-only-focusable" href="#main">
           {intl.formatMessage(messages['header.label.skip.nav'])}
@@ -100,7 +175,7 @@ const DesktopHeader = ({
         <div className={`container-fluid ${logoClasses}`}>
           <div className="nav-container position-relative d-flex align-items-center">
 
-            {/* Left: breadcrumb with current page name */}
+            {/* Left: breadcrumb */}
             <div className="site-header-top__page-title">
               <span className="site-header-breadcrumb">
                 <span className="site-header-breadcrumb__home">Home</span>
@@ -109,7 +184,7 @@ const DesktopHeader = ({
               </span>
             </div>
 
-            {/* Right: secondary menu + user (or logged-out items) */}
+            {/* Right: user info */}
             <nav
               aria-label={intl.formatMessage(messages['header.label.secondary.nav'])}
               className="nav secondary-menu-container align-items-center ml-auto"
