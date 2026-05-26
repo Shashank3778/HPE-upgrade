d15b7a2392e3abd84b954870c9e73dc315007385
Shashank3778 <anku3778@gmail.com>
2026-05-26 17:26:38 +0530

    new desing

--- src/containers/CoursesPanel/CourseList/index.jsx
+++ src/containers/CoursesPanel/CourseList/index.jsx
@@ -22,7 +22,7 @@ export const CourseList = ({ courseListData }) => {
           <ActiveCourseFilters />
         </div>
       )}
-      <div className="d-flex flex-column flex-grow-1">
+      <div className="course-list-grid">
         {visibleList.map(({ cardId }) => (
           <CourseCard key={cardId} cardId={cardId} />
         ))}
