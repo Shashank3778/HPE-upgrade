7f4ea6cadae4d7ce8ec9ae9af684dbe3c5dcd3a6
Shashank3778 <anku3778@gmail.com>
2026-06-07 16:11:46 +0530

    new desing

--- src/plugin-slots/HeaderSlot/index.jsx
+++ src/plugin-slots/HeaderSlot/index.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { PluginSlot } from '@openedx/frontend-plugin-framework';
 
-import Header from '@edx/frontend-component-header';
+import {LearningHeader as Header} from '@edx/frontend-component-header';
 const HeaderSlot = ({
   courseOrg, courseNumber, courseTitle, showUserDropdown,
 }) => (
